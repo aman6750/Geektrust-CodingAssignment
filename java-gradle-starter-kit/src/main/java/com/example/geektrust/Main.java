16e43fdab8f1712e5be6fda5c8877b6674e1c9f7
Aman <amanyadav6750@gmail.com>
2023-03-17 00:10:10 +0530

    starting project

---
+++ java-gradle-starter-kit/src/main/java/com/example/geektrust/Main.java
@@ -0,0 +1,20 @@
+package com.example.geektrust;
+
+public class Main {
+    public static void main(String[] args)  {
+        /*
+        Sample code to read from file passed as command line argument
+        try {
+            // the file to be opened for reading
+            FileInputStream fis = new FileInputStream(args[0]);
+            Scanner sc = new Scanner(fis); // file to be scanned
+            // returns true if there is another line to read
+            while (sc.hasNextLine()) {
+               //Add your code here to process input commands
+            }
+            sc.close(); // closes the scanner
+        } catch (IOException e) {
+        }
+        */
+	}
+}
